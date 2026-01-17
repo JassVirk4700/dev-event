c5c9822b3a32854e5d1a90e8d7fdeda4592cf242
Jass <jashanpreetvirk021@gmail.com>
2026-01-17 23:13:43 +0530

    feat: implement event details page via dynamic slug route

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -1,5 +1,42 @@
 import { notFound } from "next/navigation";
+import Image from "next/image";
+import BookEvent from "../../../components/BookEvent";
 
+{/* ------------------------------- Components ------------------------------- */ }
+const EventDetailItem = ({ icon, alt, label, data }: { icon: string, alt: string, label: string, data: string }) => (
+    <div className="flex items-center gap-2 my-2">
+        <Image src={icon} alt={alt} width={20} height={20} />
+        <div className="flex-col items-center">
+            <span>{label}</span>
+            <span>{data}</span>
+        </div>
+    </div>
+)
+
+const EventAgenda = ({ agendaItems }: { agendaItems: string[] }) => (
+    <div>
+        <h2 className="mb-2">Agenda</h2>
+        <ul className="agenda">
+            {agendaItems.map((item: string, index: number) => (
+                <li key={index}>{item}</li>
+            ))}
+        </ul>
+    </div>
+)
+
+const EventTags = ({ tags }: { tags: string[] }) => (
+    <div className="flex flex-row flex-wrap gap-2 my-4  ">
+        {tags.map((tag: string, index: number) => (
+            <span
+                key={index}
+                className="tag bg-[#0D161A] text-white p-4 rounded-md cursor-pointer">
+                {tag.replace(/\b\w/g, c => c.toUpperCase())}
+            </span>
+        ))}
+    </div>
+)
+
+{/* ------------------------------- Main Page ------------------------------- */ }
 const EventDetailsPage = async ({ params }: { params: Promise<{ slug: string }>; }) => {
     const { slug } = await params;
     const request = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/events/${slug}`,
@@ -8,12 +45,88 @@ const EventDetailsPage = async ({ params }: { params: Promise<{ slug: string }>;
 
     if (!request.ok) return notFound(); // Handle 4xx/5xx responses by showing 404 page
 
-    const event = await request.json();
+    const rawEvent = await request.json(); // Parse the JSON response
+    const event = rawEvent.data; // Extract the event data
+    const {
+        title,
+        description,
+        overview,
+        image,
+        venue,
+        location,
+        date,
+        time,
+        mode,
+        audience,
+        agenda,
+        organizer,
+        tags } = event;
+
+    let bookings = 50; // Example static value for available bookings
 
     return (
-        <section id="event-details">
-            <h1>Event Details</h1>
-            <h2>{event.data.title}</h2>
+        <section id="event" className="mx-10">
+            {/* ------------------------------- Event Header ------------------------------- */}
+            <div className="header mt-10">
+                <h1 className="">{title}</h1>
+                <p>{description}</p>
+            </div>
+
+            <div className="details">
+                {/* ------------------------------- Event Left Side ------------------------------- */}
+                <div className="content">
+                    {/* Image */}
+                    <Image src={image} alt={title} width={800} height={400} className="w-full h-auto rounded-xl object-cover sm:max-w-[420px] md:max-w-[600px] lg:max-w-[800px] mx-auto lg:mx-1" />
+
+                    {/* Overview */}
+                    <section className="flex-col gap-2 px-0">
+                        <h2>Overview</h2>
+                        <p>{overview}</p>
+                    </section>
+
+                    {/* Details */}
+                    <section className="flex-col gap-3">
+                        <h2>Event Details</h2>
+                        <EventDetailItem icon="/icons/calendar.svg" alt="📅" label="Date : " data={date} />
+                        <EventDetailItem icon="/icons/clock.svg" alt="⏰" label="Time : " data={time} />
+                        <EventDetailItem icon="/icons/pin.svg" alt="📍" label="Location : " data={venue + ", " + location} />
+                        <EventDetailItem icon="/icons/mode.svg" alt="📱" label="Mode : " data={mode} />
+                        <EventDetailItem icon="/icons/audience.svg" alt="👥" label="Audience : " data={audience} />
+                    </section>
+
+                    {/* Agenda */}
+                    <section>
+                        <EventAgenda agendaItems={JSON.parse(agenda[0])} />
+                    </section>
+
+                    {/* Organizer */}
+                    <section className="flex-col gap-2  ">
+                        <h2>About the Organizer</h2>
+                        <p>{organizer}</p>
+                    </section>
+
+                    {/* Tags */}
+                    <section>
+                        <EventTags tags={JSON.parse(tags[0])} />
+                    </section>
+                </div>
+
+                {/* ------------------------------- Event Right Side ------------------------------- */}
+                <aside className="booking">
+                    <div className="signup-card">
+                        <h2 className="text-2xl font-bold">Book Your Spot</h2>
+                        {bookings > 0 ? (
+                            <p className="mb-0">
+                                Hurry! Only <span className="font-bold">{bookings}</span> spots left.
+                            </p>) : (
+                            <p className="text-red-500 font-bold">
+                                Be the first to book this event!
+                            </p>
+                        )}
+                        <BookEvent />
+                    </div>
+                </aside>
+            </div>
         </section>
     )
 }
---
+++ components/BookEvent.tsx
@@ -0,0 +1,58 @@
+'use client'
+
+import React, { useState } from "react"
+
+const BookEvent = () => {
+    const [email, setEmail] = useState("");
+    const [submitted, setSubmitted] = useState(false);
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+
+        setTimeout(() => {
+            setSubmitted(true);
+        }, 1000);
+    }
+
+    return (
+        <div className="flex justify-center min-h-20">
+            <div className="w-full max-w-md rounded-2xl mt-0">
+                {submitted ? (
+                    <p className="text-sm text-emerald-300">
+                        Thank you for booking! We have received your request.
+                    </p>
+                ) : (
+                    <form onSubmit={handleSubmit} className="space-y-5">
+                        <div>
+                            <label
+                                htmlFor="email"
+                                className="block mb-2 text-sm font-medium text-slate-200"
+                            >
+                                Email Address
+                            </label>
+
+                            <input
+                                id="email"
+                                type="email"
+                                required
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                                placeholder="Enter your email"
+                                className="w-full rounded-xl border border-white/10 bg-[#0f2a2e] px-4 py-3 text-sm text-white placeholder:text-slate-400 outline-none focus:ring-2 focus:ring-emerald-400"
+                            />
+                        </div>
+
+                        <button
+                            type="submit"
+                            className="w-full rounded-xl cursor-pointer bg-linear-to-r from-emerald-400 to-teal-400 py-3 text-sm font-semibold text-[#0D161A] text-shadow-green-800 transition hover:opacity-90"
+                        >
+                            Submit
+                        </button>
+                    </form>
+                )}
+            </div>
+        </div>
+    )
+}
+
+export default BookEvent
